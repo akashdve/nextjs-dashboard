a44eff8a1401f77324d7b90afab8d9805bc75d5e
Akash Dave <akashdave555@gmail.com>
2024-04-16 11:59:12 -0400

    - Implements Accessbility, Server side form validation (Chapter 14) - Implements Authentication  using NextAuth.js (Chapter 15)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,13 +4,21 @@ import z from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer.",
+    }),
+    amount: z.coerce.number({
+        invalid_type_error: "Please enter an amount greater than $0."
+    }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: "Please select an invoice status."
+    }),
     date: z.string(),
 });
 
@@ -18,12 +26,35 @@ const CreateInvoice = FormSchema.omit({id: true, date:true});
 
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+
+//This is temporary until @types/react-dom is updated
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount ?: string[];
+        status ?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    //validate fields from Zod  
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get('status')
     });
+
+    //if form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice'
+        }
+    }
+
+    //Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -76,4 +107,24 @@ export async function deleteInvoice(id: string) {
         return { message: "Database Error. Failed to delete Invoice"};
     }
 
+}
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
 }
